0297d19ecb6f49e4b32f762c359db1173857a18c
imKateYoung <imkateyoungdev@gmail.com>
2021-06-16 12:25:54 -0400

    updates

--- kyu8/6-16-2021.js
+++ kyu8/6-16-2021.js
@@ -3,7 +3,16 @@
 
 //Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-function evenOrOdd(num){
+function evenOrOddWithTernaryOperator(num){
 	return num % 2 == 0  ? "even" : "odd"
  }
+ 
 
+function evenOrOdd(num){
+	if ( num % 2 == 0){
+		return 'Even'
+	}
+	else{
+		return 'Odd'
+	}
+}
