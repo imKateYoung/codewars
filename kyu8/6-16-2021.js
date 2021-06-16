540a254c3e99bfe493a806a215701dc851c6c2d7
imKateYoung <kateho2011@hotmail.com>
2021-06-16 10:57:20 -0400

    initial commit

---
+++ kyu8/6-16-2021.js
@@ -0,0 +1,9 @@
+// Even or even_or_odd
+// https://www.codewars.com/kata/
+
+//Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(num){
+	return num % 2 == 0  ? "even" : "odd"
+ }
+
